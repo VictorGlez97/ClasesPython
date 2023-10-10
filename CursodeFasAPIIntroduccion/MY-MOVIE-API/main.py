9b2e93d575ede3b19b09fd06ae4029aa357e2ab4
vhgonzalez <vhgonzalez@CATECINF214126D>
2023-10-09 18:56:31 -0600

    Initialize the FastAPI Class

---
+++ CursodeFasAPIIntroduccion/MY-MOVIE-API/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+movies = [
+    {
+        'id': 1,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    },
+    {
+        'id': 2,
+        'title': 'Avatar',
+        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'    
+    }
+]
+
+app = FastAPI()
+app.title = 'Mi aplicacion con FastAPI'
+app.version = '0.0.1'
+
+@app.get('/', tags=['home'])
+def message():
+    return HTMLResponse('<h1>Hello world</h1>')
+
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    
+    for item in movies:
+        if item['id'] == id:
+            return item
+
+    return []
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str):
+    return category
