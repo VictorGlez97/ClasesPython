3b8ab2e0fe376adb12c7d512ac04dac1ae6802f5
vhgonzalez <vhgonzalez@CATECINF214126D>
2023-10-10 18:49:39 -0600

    date 2023 10 10

--- CursodeFasAPIIntroduccion/MY-MOVIE-API/main.py
+++ CursodeFasAPIIntroduccion/MY-MOVIE-API/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
 movies = [
     {
@@ -16,13 +18,21 @@ movies = [
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
         'year': '2009',
         'rating': 7.8,
-        'category': 'Acción'    
+        'category': 'Drama'    
     }
 ]
 
 app = FastAPI()
 app.title = 'Mi aplicacion con FastAPI'
 app.version = '0.0.1'
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 @app.get('/', tags=['home'])
 def message():
@@ -44,4 +54,34 @@ def get_movie(id: int):
 
 @app.get('/movies/', tags=['movies'])
 def get_movies_by_category(category: str):
-    return category
+    
+    return list(filter(lambda x: x['category'] == category, movies))
+
+@app.post('/movies', tags=['movies'])
+def create_movie(movie: Movie):
+    movies.append( movie )
+    return movies
+
+@app.put('/movies', tags=['movies'])
+def update_movie(id: int, movie: Movie):
+
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
+
+    return movies
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+
+    for item in movies:
+        if item['id'] == id:
+            movies.remove(item)
+
+    return movies
+
+
