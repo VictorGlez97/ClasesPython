1f43b46713d4623924e1372095123c37ca024bc0
vhgonzalez <vhgonzalez@CATECINF214126D>
2023-11-24 18:55:24 -0600

    testing beautifoulsoup

--- Prediccion/app.py
+++ Prediccion/app.py
@@ -12,40 +12,50 @@ def get_prediccion():
 
     # BEAUTIFULSOUP
     # URL PAGINA
-    url = 'https://www.sofascore.com/es/'
+    url = 'https://www.sofascore.com/tournament/football/argentina/liga-profesional-de-futbol/155#47647'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    # HACER PETICION A PAGINA
-    response = requests.get(url)
-    print(response)
+    try:
 
-    # VERIFICAR SI SE PUDO ENTRAR A LA PAGINA
-    if response.status_code == 200:
+        # HACER PETICION A PAGINA
+        response = requests.get(url, headers=headers)
+        print(response)
 
-        # HACIENDO INSTANCIA A PLUGIN BEAUTIFULSOUP
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # VERIFICAR SI SE PUDO ENTRAR A LA PAGINA
+        if response.status_code == 200:
 
-        # IMPRIMIENDO TITULO DE LA PAGINA
-        print('Titulo de la pagina', soup.title.text)
-        
-        # BUSCANDO ELEMENTOS DE PAGINA POR MEDIO DE CLASES
-        items = soup.find_all(class_='sc-fqkvVR sc-dcJsrY')
+            # HACIENDO INSTANCIA A PLUGIN BEAUTIFULSOUP
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # print( soup.prettify() )
 
-        # ITERANDO RESULTADO DE BUSQUEDA POR CLASES
-        for item in items:
-            print('Elemento: ', item) 
+            # IMPRIMIENDO TITULO DE LA PAGINA
+            print('Titulo de la pagina', soup.title.text)
+            
+            # BUSCANDO ELEMENTOS DE PAGINA POR MEDIO DE CLASES
+            items = soup.find_all('div', class_='sc-fqkvVR')
+            # items = soup.find_all('a')
+            # print( items )
+            print( len(items) )
 
-        # SCIPY
-        # Definir la media (número promedio de eventos en el intervalo)
-        media = 3
+            # ITERANDO RESULTADO DE BUSQUEDA POR CLASES
+            for item in items:
+                print('Item: ', item) 
 
-        # Número de eventos para el cual deseas calcular la probabilidad
-        x = 2
+            # SCIPY
+            # Definir la media (número promedio de eventos en el intervalo)
+            media = 3
 
-        # Calcular la probabilidad de tener exactamente 'x' eventos
-        probabilidad_exacta = poisson.pmf(x, media)
+            # Número de eventos para el cual deseas calcular la probabilidad
+            x = 2
 
-        # Calcular la probabilidad acumulativa de tener 'x' eventos o menos
-        probabilidad_acumulativa = poisson.cdf(x, media)
- 
+            # Calcular la probabilidad de tener exactamente 'x' eventos
+            probabilidad_exacta = poisson.pmf(x, media)
+
+            # Calcular la probabilidad acumulativa de tener 'x' eventos o menos
+            probabilidad_acumulativa = poisson.cdf(x, media)
+    
+    except Exception as e:
+
+        print(e)
 
     return 'Hola'
